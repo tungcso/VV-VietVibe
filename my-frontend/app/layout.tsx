e7ab63524acfb74b6ba83e087f9d793ab6db252f
tungcso <luongtung882005@gmail.com>
2026-05-17 22:31:53 +0700

    fix mongo

--- my-frontend/app/layout.tsx
+++ my-frontend/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="ja"
-      className={`${vvSans.variable} ${vvDisplay.variable} h-full antialiased`}
+      className={`${vvSans.variable} ${vvDisplay.variable} h-full antialiased `}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
         {children}
